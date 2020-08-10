0f05c8b9926d3cdb9ad3a8c66855d101517116ea
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2020-08-10 18:36:43 +0900

    7. Testimonials-Project
    app.js = what I wrote

---
+++ 7. Testimonials-Project-master/js/app.js
@@ -0,0 +1,45 @@
+const btns = document.querySelectorAll('.btn')
+
+const customerImg = document.querySelector('#customer-img')
+const customerName = document.querySelector('#customer-name')
+const customerText = document.querySelector('#customer-text')
+
+const customerInfo = [
+    {id: "0", name: "JOHN", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?"},
+    {id: "1", name: "SANDY", text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock"},
+    {id: "2", name: "AMY", text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."},
+    {id: "3", name: "TYRELL", text: "If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."},
+    {id: "4", name: "WANDA", text: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}    
+]
+
+let index = 0
+
+btns.forEach(btn => {
+    btn.addEventListener('click', function(){
+        if(btn.classList.contains('prevBtn')){
+            index--
+            if(index < 0){
+                index = customerInfo.length - 1
+            }
+            
+            customerImg.src = "./img/customer-" + index + ".jpg"
+
+            customerName.textContent = customerInfo[index].name
+            customerText.textContent = customerInfo[index].text
+        }
+
+        if(btn.classList.contains('nextBtn')){
+            
+            index++
+            
+            if(index > 4){
+                index = 0
+            }
+
+            customerImg.src = "./img/customer-" + index + ".jpg"
+
+            customerName.textContent = customerInfo[index].name
+            customerText.textContent = customerInfo[index].text
+        }
+    })
+});
